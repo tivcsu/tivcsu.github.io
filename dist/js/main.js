517a89e96d415241e9fd393b41eda9400547dd69
Tivadar Lisch <tlisch@npsh.hu>
2021-09-28 19:38:11 +0200

    background decor lines

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,34 +1,5 @@
 $(document).ready(function () {
-  $("#page-home").on("click", function () {
-    $(".pagination__page").removeClass("active");
-    $(this).addClass("active");
-    $("html, body").animate(
-      {
-        scrollTo: $("#home").offset().left,
-      },
-      2000
-    );
-  });
-  $("#page-about").on("click", function () {
-    $(".pagination__page").removeClass("active");
-    $(this).addClass("active");
-    $("html, body").animate(
-      {
-        scrollTo: $("#about").offset().left,
-      },
-      2000
-    );
-  });
-  $("#page-contact").on("click", function () {
-    $(".pagination__page").removeClass("active");
-    $(this).addClass("active");
-    $("html, body").animate(
-      {
-        scrollLeft: $("#contact").offset().left,
-      },
-      2000
-    );
-  });
+
 
   $("#nav-home").on("click", function () {
     $(".pagination__page").removeClass("active");
