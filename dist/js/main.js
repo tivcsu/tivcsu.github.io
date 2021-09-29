8685569d53da0d47a0510583a7ec106beec46260
Tivadar Lisch <tlisch@npsh.hu>
2021-09-29 19:29:13 +0200

    images

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,12 +1,1 @@
-$(document).ready(function () {
-
-
-  $("#nav-home").on("click", function () {
-    $(".pagination__page").removeClass("active");
-    $("#page-home").addClass("active");
-  });
-  $("#nav-about").on("click", function () {
-    $(".pagination__page").removeClass("active");
-    $("#page-about").addClass("active");
-  });
-});
+$(document).on("ready", function () {});
