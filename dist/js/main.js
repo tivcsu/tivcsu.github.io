70330b57b51735e302243c0ae27f32f7d607204f
Tivadar Lisch <tlisch@npsh.hu>
2021-10-04 20:54:54 +0200

    about page content

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,11 @@
-$(document).on("ready", function () {});
+$(document).ready(function () {
+
+    $("#about-btn").on("click", function () {
+        $(".about-wrapper").addClass('active');
+        setTimeout(function () {
+            $(".page").hide();
+        }, 400);
+
+    })
+
+});
