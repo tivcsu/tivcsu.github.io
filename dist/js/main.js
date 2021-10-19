c76522162e10e8434eb4eb6a4ee89d1a6a2a58cc
Tivadar Lisch <tlisch@npsh.hu>
2021-10-20 00:28:58 +0200

    logo & scroll behaviour

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,78 @@
 $(document).ready(function () {
 
+    var $container = $('.portfolio'),
+        $activePage = $("body").attr("class"),
+        $scrollTo = $('#' + $activePage),
+        $pages = ["home", "about", "contact"],
+        $pageIndex = 0,
+        lastScrollTop = 0,
+        pageHeight = $("#about").offset().top,
+        scrollToPos = 0,
+        isScrolling = false
+
     $("#about-btn").on("click", function () {
         $(".about-wrapper").addClass('active');
         setTimeout(function () {
             $(".page").hide();
+            $("#back-to-home").show();
         }, 400);
     })
 
     $("#back-to-home").on("click", function () {
         $(".about-wrapper").removeClass('active');
         $(".page").show();
+        $("#back-to-home").hide();
+        lastScrollTop = 0;
+        $pageIndex = 0;
+        isScrolling = false;
+        scrollToPos = 0;
+        $($container).removeClass("scrollOn")
     })
+
+
+    $($container).scroll(function (event) {
+        if (!isScrolling) {
+            isScrolling = true;
+            scrollToPage();
+            $(this).addClass("scrollOn")
+        }
+        else if ($container.scrollTop() == scrollToPos) {
+            isScrolling = false
+            $(this).removeClass("scrollOn")
+            lastScrollTop = $($container).scrollTop()
+        }
+
+        return
+    })
+
+
+    //Scroll to page
+
+    function scrollToPage() {
+        var st = $($container).scrollTop();
+        console.log("last: ", lastScrollTop, "now: ", st)
+        if (st > lastScrollTop && $pageIndex < 2) {
+            lastScrollTop = st;
+            $pageIndex = $pageIndex + 1
+            $activePage = $pages[$pageIndex]
+            scrollToPos = pageHeight * ($pageIndex)
+
+        }
+        if (st < lastScrollTop && $pageIndex > 0) {
+            lastScrollTop = st;
+            $pageIndex = $pageIndex - 1
+            $activePage = $pages[$pageIndex]
+            scrollToPos = pageHeight * ($pageIndex)
+        }
+        $("body").attr("class", $activePage)
+        $scrollTo = $('#' + $activePage)
+        $container.scrollTop(
+            $scrollTo.offset().top - $container.offset().top + $container.scrollTop()
+        );
+    }
+
+
+
+
+
 });
